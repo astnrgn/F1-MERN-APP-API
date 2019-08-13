cf51dfccb482caf554be3d6642cf78b222a7e586
Austin Michael Regan <austinmregan@gmail.com>
2019-08-13 19:13:53 -0400

    ek 2

--- f1-mern-app-api/db/seed.js
+++ f1-mern-app-api/db/seed.js
@@ -1,10 +1,8 @@
 const drivers = require("./f1Data.json");
 const Driver = require("../models/Driver.js");
 
-// Driver.find({}).remove(() =>
-//   Driver.collection.insert(drivers).then(driver => {
-//     console.log(driver);
-//   })
-// );
-
-console.log("test");
+Driver.find({}).remove(() =>
+  Driver.collection.insert(drivers).then(driver => {
+    console.log(driver);
+  })
+);
