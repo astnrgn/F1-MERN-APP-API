2187d2ba763cfe2c38a6120035cd35e85a234e2e
Kyle Clabough <kaclabough@gmail.com>
2019-08-13 17:02:10 -0400

    seed.js completed

---
+++ f1-mern-app-api/db/seed.js
@@ -0,0 +1,8 @@
+const drivers = require("./f1Data.json");
+const Driver = require("../models/Driver.js");
+
+Driver.find({}).remove(() =>
+  Driver.collection.insert(drivers).then(driver => {
+    console.log(driver);
+  })
+);
